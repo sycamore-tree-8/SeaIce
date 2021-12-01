268be80fd6bf912feab2d7049e720ff2510e7ec4
sycamore-tree-8 <polina.adamovskaya@gmail.com>
2021-12-01 15:29:41 +0300

    Update read_excel_data.py

--- read_excel_data.py
+++ read_excel_data.py
@@ -5,37 +5,63 @@ Created on Wed Nov 17 15:50:37 2021
 @author: Professional
 """
 
-import pandas as pd
-import matplotlib.pyplot as plt
+import calendar
 from datetime import datetime
 
-df = pd.read_excel('../idata/kara_obs_8st.xlsx') #, dtype='int32')
-print(df)
+import pandas as pd
+import matplotlib.pyplot as plt
 
-#построить графики
-#перевести даты
+meta = pd.read_excel('../idata/db_ice_stations.xlsx', sheet_name=None)
+print(meta)
 
-y1 = df['FI']
-y2 = df['AD']
-y3 = df['LD']
-y4 = df['CC']
-x = df.index
+assert 1==2
 
-print(df.describe())
+df_dict = pd.read_excel('../idata/kara_obs_8st.xlsx', sheet_name=None) #, dtype='int32')
 
-plt.plot(x, y1, label='FI')
-plt.plot(x, y2, label='AD', alpha=0.5)
-plt.plot(x, y3, label='LD')
-plt.plot(x, y4, label='CC')
-plt.xlabel('frequency')
-plt.ylabel('DOY')
-plt.legend()
-plt.show
-  
-cyear = df['YEAR']
-doy = df['FI']    
-full_date = pd.to_datetime('{}{}'.format(cyear, doy), format='%Y%j%d')
-print(full_date)
+for key in sorted(list(df_dict.keys()))[:]:
+    df = df_dict[key]
+    print(df)
 
-#full_date = df['FI'].to_datetime('{}{}'.format(cyear, doy), format='%Y%j')
-#print(full_date)
+
+
+    #построить графики
+    #перевести даты
+    
+    y1 = df['FI']
+    y2 = df['AD']
+    y3 = df['LD']
+    y4 = df['CC']
+    x = df.index
+    
+    print(df.describe())
+    
+    fig = plt.figure(figsize=(8, 6))
+    
+    plt.plot(x, y1, label='FI')
+    plt.plot(x, y2, label='AD', alpha=0.5)
+    plt.plot(x, y3, label='LD')
+    plt.plot(x, y4, label='CC')
+    plt.xlabel('frequency')
+    plt.ylabel('DOY')
+    plt.title('{}'.format(key))
+    plt.legend()
+    
+    plt.show()
+    
+    new = df.dropna()
+    new = new[['FI', 'YEAR']].copy()
+    
+    for i, row in new.iterrows():
+        if calendar.isleap(row['YEAR']):
+            tr = 366
+        else:
+            tr = 365
+        if row['FI'] > tr:
+            new.loc[i, 'YEAR'] += 1
+            new.loc[i, 'FI'] -= tr 
+    
+    
+    cyear = new['YEAR'].astype('int32').values
+    doy = new['FI'].astype('int').values
+    box = [str('{}{}'.format(iyear, idoy)) for iyear, idoy in zip(cyear, doy)]
+    full_date = pd.to_datetime(box, format='%Y%j')
