52771d544f68d739d3165adbf09a4751e7bea4d8
sycamore-tree-8 <polina.adamovskaya@gmail.com>
2021-12-06 11:30:41 +0300

    Create gpd_starter_ice.py

---
+++ gpd_starter_ice.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  5 15:00:32 2021
+
+@author: Professional
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+import geopandas
+
+gdf = geopandas.read_file(r"C:\Users\Professional\Sea_ice\aari_arc_20211130_pl_a.shp")
