bae5dc3532b966a8fdcfde5f8898cb5c4526583e
sycamore-tree-8 <polina.adamovskaya@gmail.com>
2021-12-16 12:39:09 +0300

    extarct files

--- unzip_sigrid.py
+++ unzip_sigrid.py
@@ -7,20 +7,23 @@ Created on Mon Dec 13 14:37:56 2021
 
 import os
 import zipfile
-import pandas as pd
+#import pandas as pd
 
 years = range(1997, 2022)
 
 for cyear in years[:1]:
     
-    files_list = os.listdir('./{}'.format(cyear))
+    zip_files = '../idata/zipped_data/{}'.format(cyear)
     
-    if not os.path.exists(str(cyear)):
-        os.makedirs('./unziped/{}'.format(cyear))
+    files_list = os.listdir(zip_files)
+    
+    to_unzip_path = '../idata/unzipped/{}'.format(cyear)
+    if not os.path.exists(to_unzip_path):
+        os.makedirs(to_unzip_path)
         
     for f in files_list:
         
-        with zipfile.ZipFile("./{}/{}".format(cyear, f), 'r') as zip_ref:
-            zip_ref.extractall('./unziped/{}'.format(cyear))
+        with zipfile.ZipFile("{}/{}".format(zip_files, f), 'r') as zip_ref:
+            zip_ref.extractall(to_unzip_path)
         
     
